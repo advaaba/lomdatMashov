cde3d7a56419e074645b932718e100cc78ff09bc
madortill <mador.till@gmail.com>
2025-07-01 13:11:04 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -12,5 +11,4 @@ export default defineConfig({
     }
   },
   base: process.env.NODE_ENV === "production" ? "/lomdatMashov/" : "/",
-  // base: './',
 })
